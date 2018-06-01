09c3d645096254cd3f6bc75a2bb47697f5a9af5a
Serhii Herasymovych <serhiiherasymovych@gmail.com>
2018-06-01 18:00:06 +0300

    cream

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+$(document).ready(function () {
+    $('[data-fancybox]').fancybox({
+        protect: true,
+        buttons: false,
+        speed: 300,
+        loop: true,
+        smallBtn: 'auto',
+    });
+})
